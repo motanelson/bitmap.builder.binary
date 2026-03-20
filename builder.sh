23a2d775ddce21abbb6dd68a2c29ddf03cb85ba4
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-03-20 20:49:48 +0000

    Add files via upload

---
+++ builder.sh
@@ -0,0 +1,9 @@
+rm *.o
+rm *.elf
+rm *.img
+/bin/aarch64-linux-gnu-as  -o boot.o boot.S
+/bin/aarch64-linux-gnu-gcc-13 -c -nostdlib -o kernel.o kernel.c -ffreestanding
+/bin/aarch64-linux-gnu-ld  -T link.ld boot.o kernel.o -o kernel.elf -nostdlib
+/bin/aarch64-linux-gnu-objcopy kernel.elf -O binary kernel.img
+qemu-system-arm64 -m 1G -machine raspi3b -serial stdio -kernel kernel.elf
+
